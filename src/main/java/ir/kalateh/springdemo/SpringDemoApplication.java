064ebc04d25783106d59cc0a329430d1ae4bd413
hosseinam <hks@pnr.ir>
2022-09-22 21:02:32 +0330

    add applicationContext.xml -> xml config

--- src/main/java/ir/kalateh/springdemo/SpringDemoApplication.java
+++ src/main/java/ir/kalateh/springdemo/SpringDemoApplication.java
@@ -2,14 +2,19 @@ package ir.kalateh.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 	
 	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		SpringApplication.run(SpringDemoApplication.class, args);
-		Coach theCoach = new TrackCoach();
+		Coach theCoach = (Coach) context.getBean("baseballCoach");
 		System.out.println(theCoach.getDailyWorkout());
+		context.close();
+		
 	}
 	
 }
