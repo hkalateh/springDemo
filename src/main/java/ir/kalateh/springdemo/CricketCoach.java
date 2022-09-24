1433dd6d85f00697a89fb893a9451fe0f3512ebe
hosseinam <hks@pnr.ir>
2022-09-24 17:21:19 +0330

    set letteral value with config file setter injection, close #2

--- src/main/java/ir/kalateh/springdemo/CricketCoach.java
+++ src/main/java/ir/kalateh/springdemo/CricketCoach.java
@@ -3,10 +3,11 @@ package ir.kalateh.springdemo;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	private String emailAddress;
 	
 	@Override
 	public String getDailyWorkout() {
-		return "practice fast bowling for 15 minutes";
+		return "practice fast bowling for 15 minutes, if you have any question please deal with us with email: " + emailAddress;
 	}
 	
 	@Override
@@ -16,5 +17,9 @@ public class CricketCoach implements Coach {
 	
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
 	}
 }
