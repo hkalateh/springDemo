cc3e3ac45afdc1ca9b3073019de6f7e8dfba0e34
hosseinam <hks@pnr.ir>
2022-09-25 20:43:08 +0330

    define init-method and destroy-method and add in config file(applicationContext.xml), close #4

--- src/main/java/ir/kalateh/springdemo/CricketCoach.java
+++ src/main/java/ir/kalateh/springdemo/CricketCoach.java
@@ -16,6 +16,14 @@ public class CricketCoach implements Coach {
 		return "let's do it: " + fortuneService.getFortune();
 	}
 	
+	public void myInitMethod(){
+		System.out.println("in cricket coach : in the init method");
+	}
+	
+	public void myDestroyMethod(){
+		System.out.println("in cricket coach : in the destroy method");
+	}
+	
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
